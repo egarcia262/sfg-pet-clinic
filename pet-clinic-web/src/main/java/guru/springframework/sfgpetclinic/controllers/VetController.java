d2682f69ca669ea5497fc07867c78e451c33505e
Edu <garcia_eduher@gva.es>
2022-07-27 09:34:24 +0200

    Adding not implemented page

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import guru.springframework.sfgpetclinic.services.VetService;
 
-@RequestMapping("/vets")
 @Controller
 public class VetController {
 
@@ -16,7 +15,7 @@ public class VetController {
 		this.vetService = vetService;
 	}
 
-	@RequestMapping({ "", "/index", "/index.html" })
+	@RequestMapping({"/vets", "/index", "/vets/index.html", "/vets.html"})
 	public String listVets(Model model){
 
         model.addAttribute("vets", vetService.findAll());
